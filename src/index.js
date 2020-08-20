0a0211755f0fece599ade47e83917b9d5c89b889
Trevor Alt <trevorjalt@gmail.com>
2020-08-19 21:43:33 -0400

    wireframe

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
 
+import 'normalize.css';
+import './css/index.css';
+
 function main() {
   console.log('DOM is loaded');
 
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
+//   const startMsg = $('<p>Webpack is working!</p>');
+//   $('#root').append(startMsg);
 }
 
 $(main);
